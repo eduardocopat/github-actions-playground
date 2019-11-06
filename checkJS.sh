0a8a58b07fc175a49c104737b043bcf04e89dfd8
Eduardo Ferrari Copat <ecopat@gmail.com>
2019-11-06 15:56:39 +1300

    Update checkJS.sh

--- checkJS.sh
+++ checkJS.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 echo "test"
-exit_code = 0
+exit_code=0
 
 if [ ! grep --color=auto --recursive -P -n --include *.js --exclude-dir node_modules "hello" src/ ]
 then 
-   exit_code = exit_code + 1; 
+   exit_code=exit_code + 1; 
 fi
 
 if grep --color=auto --recursive -P -n --include *.js --exclude-dir node_modules "ciao" src/
 then 
    echo "OK";
 else
-  exit_code = exit_code + 1; 
+  exit_code=exit_code + 1; 
 fi
