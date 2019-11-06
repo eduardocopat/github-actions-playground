5c29a96cbaab887a0694f183e37e155ecef4d85f
Eduardo Ferrari Copat <ecopat@gmail.com>
2019-11-06 15:44:04 +1300

    Update checkJS.sh

--- checkJS.sh
+++ checkJS.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
 echo "test"
-grep --color=auto --recursive -P -n --include *.js --exclude-dir node_modules "hello" src/
 
+if grep --color=auto --recursive -P -n --include *.js --exclude-dir node_modules "hello" src/
+then 
+   echo "OK";
+else
+   echo "NOT OK";
+fi
+
+if grep --color=auto --recursive -P -n --include *.js --exclude-dir node_modules "ciao" src/
+then 
+   echo "OK";
+else
+   echo "NOT OK";
+fi
